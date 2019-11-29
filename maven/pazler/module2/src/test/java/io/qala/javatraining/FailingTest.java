cddd0898880cb7aef53d417d0a0d21525b196f5f
Mikhail Sidnev <Mikhail_Sidnev@epam.com>
2019-11-29 23:48:47 +0300

    probably better solution for puzzler

--- maven/pazler/module2/src/test/java/io/qala/javatraining/FailingTest.java
+++ maven/pazler/module2/src/test/java/io/qala/javatraining/FailingTest.java
@@ -3,13 +3,16 @@ package io.qala.javatraining;
 
 import org.testng.annotations.Test;
 
-import java.io.FileInputStream;
+import java.io.InputStream;
 
 import static org.junit.Assert.assertEquals;
 
 public class FailingTest {
-    @Test public void strangeFailure() throws Exception {
-        String file = ClassLoader.getSystemResource("myresource.txt").getFile();
-        assertEquals(new FileInputStream(file).read(), 49);
+
+    @Test
+    @SuppressWarnings("all")
+    public void strangeFailure() throws Exception {
+        InputStream file = ClassLoader.getSystemResourceAsStream("myresource.txt");
+        assertEquals(file.read(), 49);
     }
 }
