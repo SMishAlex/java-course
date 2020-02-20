17a9fd62f5039f2167361ff238459226d640d527
Mikhail Sidnev <Mikhail_Sidnev@epam.com>
2020-02-20 10:27:33 +0300

    return statuses

--- Spring & REST/src/main/java/com/epam/javacource/sid/spring/exceptions/BeanValidationException.java
+++ Spring & REST/src/main/java/com/epam/javacource/sid/spring/exceptions/BeanValidationException.java
@@ -1,5 +1,9 @@
 package com.epam.javacource.sid.spring.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.BAD_REQUEST)
 public class BeanValidationException extends RuntimeException {
 
     public BeanValidationException(String message) {
--- Spring & REST/src/main/java/com/epam/javacource/sid/spring/exceptions/DatabaseCommunicationException.java
+++ Spring & REST/src/main/java/com/epam/javacource/sid/spring/exceptions/DatabaseCommunicationException.java
@@ -1,5 +1,9 @@
 package com.epam.javacource.sid.spring.exceptions;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.INTERNAL_SERVER_ERROR)
 public class DatabaseCommunicationException extends RuntimeException {
 
     public DatabaseCommunicationException(String message, Throwable cause) {
