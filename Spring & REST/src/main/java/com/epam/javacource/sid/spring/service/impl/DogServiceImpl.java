29d2e05e4faed07810dcd0ecf9686b678c923fce
Mikhail Sidnev <Mikhail_Sidnev@epam.com>
2020-04-01 23:49:38 +0300

    use @Transactional annotation

--- Spring & REST/src/main/java/com/epam/javacource/sid/spring/service/impl/DogServiceImpl.java
+++ Spring & REST/src/main/java/com/epam/javacource/sid/spring/service/impl/DogServiceImpl.java
@@ -5,6 +5,8 @@ import com.epam.javacource.sid.spring.dao.Dao;
 import com.epam.javacource.sid.spring.model.Dog;
 import com.epam.javacource.sid.spring.model.DogDto;
 import com.epam.javacource.sid.spring.service.DogService;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 public class DogServiceImpl implements DogService {
 
@@ -15,25 +17,25 @@ public class DogServiceImpl implements DogService {
     }
 
     @Override
-    @IDontUseTryWithResourcesPleaseLetMeBeTransactional
+    @Transactional
     public DogDto create(DogDto dog) {
         return dogDao.create(Dog.toEntity(dog)).toDto();
     }
 
     @Override
-    @IDontUseTryWithResourcesPleaseLetMeBeTransactional
+    @Transactional(readOnly = true, propagation = Propagation.SUPPORTS)
     public DogDto getOne(Integer id) {
         return dogDao.getOne(id).toDto();
     }
 
     @Override
-    @IDontUseTryWithResourcesPleaseLetMeBeTransactional
+    @Transactional
     public DogDto update(DogDto dog) {
         return dogDao.update(Dog.toEntity(dog)).toDto();
     }
 
     @Override
-    @IDontUseTryWithResourcesPleaseLetMeBeTransactional
+    @Transactional
     public void delete(Integer id) {
         dogDao.delete(id);
     }
