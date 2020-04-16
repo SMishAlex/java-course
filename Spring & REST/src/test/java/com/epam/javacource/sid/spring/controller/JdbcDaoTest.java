c231bb018e56e005493211ebbb1be1c1dc07bcd8
Mikhail Sidnev <Mikhail_Sidnev@epam.com>
2020-04-16 10:06:21 +0300

    add JNDI test support

--- Spring & REST/src/test/java/com/epam/javacource/sid/spring/controller/JdbcDaoTest.java
+++ Spring & REST/src/test/java/com/epam/javacource/sid/spring/controller/JdbcDaoTest.java
@@ -3,22 +3,65 @@ package com.epam.javacource.sid.spring.controller;
 import com.epam.javacource.sid.spring.dao.Dao;
 import com.epam.javacource.sid.spring.exceptions.DatabaseCommunicationException;
 import com.epam.javacource.sid.spring.model.Dog;
+import org.apache.tomcat.jdbc.pool.DataSourceFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 import org.testng.util.Strings;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.time.LocalDate;
+import java.util.Properties;
 
-@Test
-@ContextConfiguration(locations = "classpath:spring-mvc-config.xml")
+@ContextConfiguration(locations = {"classpath*:spring-mvc-config.xml"})
 @WebAppConfiguration
 public class JdbcDaoTest extends AbstractTestNGSpringContextTests {
 
     @Autowired
     Dao<Dog> dogDao;
+
+    @BeforeTest
+    public static void setUpDataSource() throws Exception {
+        try {
+            System.setProperty(Context.INITIAL_CONTEXT_FACTORY, "org.apache.naming.java.javaURLContextFactory");
+            System.setProperty(Context.URL_PKG_PREFIXES, "org.apache.naming");
+
+            final DataSourceFactory dataSourceFactory = new DataSourceFactory();
+            final Properties properties = new Properties();
+            properties.put("name", "jdbc/DatabaseName");
+            properties.put("auth", "Container");
+            properties.put("type", "javax.sql.DataSource");
+            properties.put("username", "dog");
+            properties.put("password", "dog");
+            properties.put("url", "jdbc:postgresql://localhost:5432/dog");
+            properties.put("driverClassName", "org.postgresql.Driver");
+            properties.put("initialSize", "20");
+            properties.put("maxWaitMillis", "15000");
+            properties.put("maxTotal", "75");
+            properties.put("maxIdle", "20");
+            properties.put("maxAge", "7200000");
+            properties.put("testOnBorrow", "true");
+            properties.put("validationQuery", "select 1");
+            final DataSource dataSource = dataSourceFactory.createDataSource(properties);
+
+            InitialContext ic = new InitialContext();
+
+            ic.createSubcontext("java:comp");
+            ic.createSubcontext("java:comp/env");
+            ic.createSubcontext("java:comp/env/jdbc");
+            ic.createSubcontext("java:comp/env/jdbc/LocalDatabaseName");
+
+            ic.rebind("java:comp/env/jdbc/LocalDatabaseName", dataSource);
+        } catch (NamingException ex) {
+            ex.printStackTrace();
+        }
+    }
 
     @Test
     public void whenDogIsValidNoExceptionsProvided() {
